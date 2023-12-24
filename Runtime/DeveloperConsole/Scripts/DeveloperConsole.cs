2619802a6bd034f68e7c01a7438dcbcc2fa050e4
Abhi86Shek <sonkar.abhishek86@gmail.com>
2023-12-24 23:57:03 +0530

    dev tool

---
+++ Runtime/DeveloperConsole/Scripts/DeveloperConsole.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SAS.Utilities.DeveloperConsole
+{
+    public class DeveloperConsole
+    {
+        private readonly string prefix;
+        private readonly IEnumerable<IConsoleCommand> commands;
+
+        public DeveloperConsole(string prefix, IEnumerable<IConsoleCommand> commands)
+        {
+            this.prefix = prefix;
+            this.commands = commands;
+        }
+
+        public void ProcessCommand(string inputValue, DeveloperConsoleBehaviour developerConsole)
+        {
+            if (!inputValue.StartsWith(prefix)) { return; }
+
+            inputValue = inputValue.Remove(0, prefix.Length);
+
+            string[] inputSplit = inputValue.Split(' ');
+
+            string commandInput = inputSplit[0];
+            string[] args = inputSplit.Skip(1).ToArray();
+
+            ProcessCommand(commandInput, args, developerConsole);
+        }
+
+        public void ProcessCommand(string commandInput, string[] args, DeveloperConsoleBehaviour developerConsole)
+        {
+            foreach (var command in commands)
+            {
+                if (!commandInput.Equals(command.CommandWord, StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+
+                if (command.Process(args,developerConsole))
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/DeveloperConsole/Scripts/DeveloperConsoleBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using TMPro;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace SAS.Utilities.DeveloperConsole
+{
+    public class DeveloperConsoleBehaviour : MonoBehaviour
+    {
+        [SerializeField] private string m_Prefix = string.Empty;
+        [SerializeField] private ConsoleCommand[] m_Commands = new ConsoleCommand[0];
+
+        [Header("UI")]
+        [SerializeField] private GameObject m_UiCanvas = null;
+        [SerializeField] private TMP_InputField m_InputField = null;
+        [SerializeField] private bool m_PauseOnOpen = false;
+
+        private float pausedTimeScale;
+
+        private DeveloperConsole developerConsole;
+        private DeveloperConsole DeveloperConsole
+        {
+            get
+            {
+                if (developerConsole != null) { return developerConsole; }
+                return developerConsole = new DeveloperConsole(m_Prefix, m_Commands);
+            }
+        }
+
+        public void Toggle(CallbackContext context)
+        {
+            if (!context.action.triggered) { return; }
+
+            if (m_UiCanvas.activeSelf)
+            {
+                if (m_InputField != null)
+                    Time.timeScale = pausedTimeScale;
+                m_UiCanvas.SetActive(false);
+            }
+            else
+            {
+                if (m_PauseOnOpen)
+                {
+                    pausedTimeScale = Time.timeScale;
+                    Time.timeScale = 0;
+                }
+                m_UiCanvas.SetActive(true);
+                m_InputField.ActivateInputField();
+            }
+        }
+
+        public void ProcessCommand(string inputValue)
+        {
+            DeveloperConsole.ProcessCommand(inputValue, this);
+            m_InputField.text = string.Empty;
+        }
+    }
+}
