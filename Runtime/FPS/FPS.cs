fe326027ae096c524fa337bfb0f67fd5ed512453
Abhi86shek <sonkar.abhishek86@gmail.com>
2025-08-04 21:44:15 +0530

    few clean up

--- Runtime/FPS/FPS.cs
+++ Runtime/FPS/FPS.cs
@@ -4,37 +4,52 @@ using UnityEngine.UI;
 
 public class FPS : UIBehaviour
 {
+    [Header("Display")]
     [SerializeField] private Text m_Display = default;
     [SerializeField] private float m_UpdateInterval = 0.5f;
     [SerializeField] private int m_TargetFrameRate = 60;
 
-    private float mAccumulatedFPS = 0;
-    private int mFrames = 0;
-    private float mTimeLeft;
-    private float mAvarageFPS = 0;
+    private float _timeLeft;
+    private int _frames;
+    private float _accumulatedFPS;
 
     protected override void Start()
     {
-        mTimeLeft = m_UpdateInterval;
+        _timeLeft = m_UpdateInterval;
         Application.targetFrameRate = m_TargetFrameRate;
     }
 
     private void Update()
     {
-        mTimeLeft -= Time.deltaTime;
-        mAccumulatedFPS += Time.timeScale / Time.deltaTime;
-        ++mFrames;
+        _timeLeft -= Time.deltaTime;
+        _accumulatedFPS += Time.timeScale / Time.deltaTime;
+        _frames++;
 
-        if(mTimeLeft <= 0)
+        if (_timeLeft <= 0)
         {
-            mAvarageFPS = mAccumulatedFPS / mFrames;
-            m_Display.text = System.String.Format("{0:F1} FPS", mAvarageFPS);
-            m_Display.color = mAvarageFPS < 30 ? mAvarageFPS < 10 ? Color.red : Color.yellow : Color.green;
+            float avgFps = _accumulatedFPS / _frames;
+            float frameTimeMs = 1000f / Mathf.Max(avgFps, 0.0001f);
+            float targetFrameTime = 1000f / m_TargetFrameRate;
 
-            mAccumulatedFPS = 0;
-            mFrames = 0;
-            mAvarageFPS = 0;
-            mTimeLeft = m_UpdateInterval;
+            string Colorize(string label, float value, string unit, Color baseColor)
+            {
+                Color color = value > targetFrameTime ? Color.red : baseColor;
+                string hex = ColorUtility.ToHtmlStringRGB(color);
+                return $"<color=#{hex}>{label}: {value:F2} {unit}</color>";
+            }
+
+            // FPS color
+            Color fpsColor = avgFps < 30 ? (avgFps < 10 ? Color.red : Color.yellow) : Color.green;
+            string fpsHex = ColorUtility.ToHtmlStringRGB(fpsColor);
+            string fpsLine = $"<color=#{fpsHex}>FPS: {avgFps:F1}</color>";
+
+            m_Display.text =
+                $"{fpsLine}\n" +
+                $"{Colorize("Frame Time", frameTimeMs, "ms", Color.white)}";
+
+            _accumulatedFPS = 0;
+            _frames = 0;
+            _timeLeft = m_UpdateInterval;
         }
     }
 }
