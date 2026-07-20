50f9a9c676af3abe1f38a413bd3660489c94e7aa
Abhishek Sonkar <sonkar.abhishek86@gmail.com>
2026-07-20 18:54:23 +0530

    script define symbol

---
+++ Editor/DefineSymbolsMenu.StateMachineDebug.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+
+namespace SAS.Utilities.DeveloperConsole.Editor
+{
+    public static partial class DefineSymbolsMenu
+    {
+        [MenuItem("Tools/DevUtilities/Defines/STATE_MACHINE_DEBUG")]
+        private static void Toggle_STATE_MACHINE_DEBUG() => ToggleDefine("STATE_MACHINE_DEBUG");
+
+        [MenuItem("Tools/DevUtilities/Defines/STATE_MACHINE_DEBUG", true)]
+        private static bool Validate_STATE_MACHINE_DEBUG() => ValidateToggle("STATE_MACHINE_DEBUG");
+    }
+}
